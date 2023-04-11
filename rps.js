7eb143e1f011a471a1a94b056eae376ffe929c76
Asim <112316210+awsimness@users.noreply.github.com>
2023-04-12 00:10:58 +0500

    Fix functions to return choices in lowercase

--- rps.js
+++ rps.js
@@ -2,6 +2,15 @@
 const choices =['Rock','Paper','Scissors']
 
 function getComputerChoice () {
-    return choices[Math.floor(Math.random()*3)];
+    return choices[Math.floor(Math.random()*3)].toLowerCase();
 }
 
+function getPlayerChoice() {
+    let playerchoice=prompt("Hi there. You have three options to choose from.\nRock\nPaper\nScissors")
+    return playerchoice.toLowerCase()
+}
+
+function playOneRound() {
+        computerchoice=getComputerChoice()
+        playerSelection=getPlayerChoice()
+}
