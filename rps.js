a3230bc0fa364e3a903ff7854fc4c8cfdecf9d65
Asim <112316210+awsimness@users.noreply.github.com>
2023-04-17 22:00:58 +0500

    Remove useless functions and make game functional upto five rounds at least by redefining the game function and the playoneround function

--- rps.js
+++ rps.js
@@ -1,18 +1,15 @@
 
-const choices =['Rock','Paper','Scissors']
+const choices =['rock','paper','scissors']
+let playerScore=0;
+let computerScore=0;
 
 function getComputerChoice () {
     return choices[Math.floor(Math.random()*3)].toLowerCase();
 }
 
-function getPlayerChoice() {
-    let playerchoice=prompt("Hi there. You have three options to choose from.\nRock\nPaper\nScissors")
-    return playerchoice.toLowerCase()
-}
-
 function playOneRound(playerSelection,computerSelection) {
         computerSelection=getComputerChoice()
-        playerSelection=getPlayerChoice()
+        playerSelection=prompt("Choose your poison : \n1. Rock \n2. Paper \n3. Scissors")
 
         console.log(`Computer chose ${computerSelection} and Player chose ${playerSelection}.`)
 
@@ -20,11 +17,26 @@ function playOneRound(playerSelection,computerSelection) {
         (computerSelection == "scissors" && playerSelection == "paper") ||
         (computerSelection == "paper" && playerSelection == "rock")) {
             console.log("Computer wins!")
+            computerScore++;
         }
         else if (computerSelection==playerSelection) {
             console.log("Tie game!")
         }
         else {
             console.log("Player wins!")
+            playerScore++;
         }
 };
+
+function game() {
+    for (i=0;i<5;i++) {
+        playOneRound()
+    }
+
+    if (playerScore>computerScore) {
+        console.log("After numerous battles, the winner of the war is Player!")
+    }
+    else {
+        console.log("After numerous battles, the winner of the war is Computer!")
+    }
+}
